ce49edf1862a79c113f47f75c2f035e5987ea862
dams <damien.carrier95@gail.com>
2022-03-01 19:48:42 +0100

    feat(mysql) : correcting third request

--- client/src/components/Footer.tsx
+++ client/src/components/Footer.tsx
@@ -1,9 +1,10 @@
 import React from 'react';
 
 const Footer = () => (
-  <div className="App-footer">
-    <p>© — Front by Léon Bousquet — Damien Carrier — Arthur Duca — Clément Savinaud</p>
-  </div>
+    <div className="App-footer">
+        <p>© — Front by Léon Bousquet — Damien Carrier — Arthur Duca — Clément Savinaud</p>
+        <p>© — Back-End by Damien Carrier — Clément Savinaud</p>
+    </div>
 );
 
 export default Footer;
